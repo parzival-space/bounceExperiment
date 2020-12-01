949c72af9a5c27ffa8b882bf2a5c4495e7f6b31f
Malte Linke <malte.linke.ml@gmail.com>
2020-12-01 20:54:59 +0100

    reformated

--- bounce.js
+++ bounce.js
@@ -3,7 +3,7 @@ console.log(`%c[Client]`, `color: cyan;`, `bounce.js loaded!`);
 (() => {
 
     var __ = document.getElementById("canvas");
-    var _  = __.getContext("2d");
+    var _ = __.getContext("2d");
 
     var EXPERIMENTAL = {
         BBTest: {
@@ -17,22 +17,22 @@ console.log(`%c[Client]`, `color: cyan;`, `bounce.js loaded!`);
     __.setAttribute("height", window.innerHeight);
     __.setAttribute("width", window.innerWidth);
     //#endregion
-    
-    var getRnd  = (min, max) => { return Math.floor((Math.random() * max) + min); };
 
-    var TPS     = 100;
-    var __BALL   = {
-        x:  getRnd(__.width / 4, (__.width / 4) * 3),
-        y:  getRnd(__.height / 4, (__.height / 4) * 3),
-        x:  __.width / 2 - 50,
-        y:  __.height / 2,
-        r:  15,
+    var getRnd = (min, max) => { return Math.floor((Math.random() * max) + min); };
+
+    var TPS = 100;
+    var __BALL = {
+        x: getRnd(__.width / 4, (__.width / 4) * 3),
+        y: getRnd(__.height / 4, (__.height / 4) * 3),
+        x: __.width / 2 - 50,
+        y: __.height / 2,
+        r: 15,
         dx: getRnd(-15, 15),
         dy: getRnd(-15, 15),
         color: `rgba(255,0,0,1)`
     };
     var CURHIGH = "∞";
-    var BALLS   = [];
+    var BALLS = [];
 
     //#region KEY EVENTS
     document.addEventListener("keydown", (e) => {
@@ -72,7 +72,7 @@ console.log(`%c[Client]`, `color: cyan;`, `bounce.js loaded!`);
     var __fullCross = true;
     var drawDebugStuff = () => {
         _.beginPath();
-        
+
         // X-LINE
         _.rect(
             0,
@@ -93,15 +93,15 @@ console.log(`%c[Client]`, `color: cyan;`, `bounce.js loaded!`);
         _.fill();
 
         _.closePath();
-        
+
         if (!__fullCross) {
             _.beginPath();
 
-            _.clearRect(0,0,__.width,(__.height/9)*4);
-            _.clearRect(0,(__.height/9)*5,__.width,(__.height/9)*4);
-            _.clearRect(0,0,(__.width/14)*6,__.height);
-            _.clearRect((__.width/14)*8,0,__.width,__.height);
-            
+            _.clearRect(0, 0, __.width, (__.height / 9) * 4);
+            _.clearRect(0, (__.height / 9) * 5, __.width, (__.height / 9) * 4);
+            _.clearRect(0, 0, (__.width / 14) * 6, __.height);
+            _.clearRect((__.width / 14) * 8, 0, __.width, __.height);
+
             _.closePath();
         }
     };
@@ -122,10 +122,10 @@ console.log(`%c[Client]`, `color: cyan;`, `bounce.js loaded!`);
                 if (!__randomMode) BALL.dy = -BALL.dy;
                 else BALL.dy = -(BALL.dy - getRnd(-3, 3))
             }
-            if (((BALL.x + BALL.r) >= __.width)  || ((BALL.x - BALL.r) <= 0)) {
+            if (((BALL.x + BALL.r) >= __.width) || ((BALL.x - BALL.r) <= 0)) {
                 if ((BALL.x - BALL.r) <= 0) BALL.x = 1 + BALL.r;
                 else BALL.x = __.width - BALL.r - 1;
-                
+
                 if (!__randomMode) BALL.dx = -BALL.dx;
                 else BALL.dx = -(BALL.dx - getRnd(-3, 3))
             }
@@ -144,18 +144,18 @@ console.log(`%c[Client]`, `color: cyan;`, `bounce.js loaded!`);
                     ) {
                         BALL.dy = -BALL.dy;
                     }
-                    else 
-                    if (
-                        (bid !== i) && (
-                            (
-                                ((BALL.y - BALL.r) >= (B.y - B.r)) && ((BALL.y + BALL.r) <= (B.y + B.r))
-                            ) && (
-                                ((BALL.x + BALL.r) >= (B.x - B.r)) && ((BALL.x - BALL.r) <= (B.x + B.r))
+                    else
+                        if (
+                            (bid !== i) && (
+                                (
+                                    ((BALL.y - BALL.r) >= (B.y - B.r)) && ((BALL.y + BALL.r) <= (B.y + B.r))
+                                ) && (
+                                    ((BALL.x + BALL.r) >= (B.x - B.r)) && ((BALL.x - BALL.r) <= (B.x + B.r))
+                                )
                             )
-                        )
-                    ) {
-                        BALL.dx = -BALL.dx;
-                    }
+                        ) {
+                            BALL.dx = -BALL.dx;
+                        }
                 })
             }
 
@@ -193,23 +193,23 @@ console.log(`%c[Client]`, `color: cyan;`, `bounce.js loaded!`);
 
     //#region ADD BALL
     var addBall = () => {
-        var b   = Object.create(__BALL);
+        var b = Object.create(__BALL);
 
-        b.x     = __.width / 2;
-        b.y     = __.height / 2;
+        b.x = __.width / 2;
+        b.y = __.height / 2;
 
-        b.dx    = getRnd(-10, 10);
-        b.dy    = getRnd(-10, 10);
+        b.dx = getRnd(-10, 10);
+        b.dy = getRnd(-10, 10);
 
-        b.r     = getRnd(8, 30);
+        b.r = getRnd(8, 30);
 
-        b.color = `rgb(${getRnd(10,255)},${getRnd(10,255)},${getRnd(10,255)})`;
+        b.color = `rgb(${getRnd(10, 255)},${getRnd(10, 255)},${getRnd(10, 255)})`;
         BALLS.push(b);
 
         return console.log(`%c[App]`, `color: orange;`, `active balls: ${BALLS.length} (+1)`);
     }
     //#endregion
-    
+
     //#region REMOVE BALL
     var removeBall = () => {
         if (BALLS.length <= 0) return console.log(`%c[App]`, `color: orange;`, `active balls: ${BALLS.length} (-0)`);
@@ -233,13 +233,13 @@ console.log(`%c[Client]`, `color: cyan;`, `bounce.js loaded!`);
     //#endregion
 
     //#region FPS COUNTER
-    var __fpsCache      = 0;
-    var __fpsCount      = 0;
-    var __fpsLast       = 0;
+    var __fpsCache = 0;
+    var __fpsCount = 0;
+    var __fpsLast = 0;
     var __viewControls = true;
-    var __waveMode     = false;
-    var __randomMode    = false;
-    var __crazyMode      = false;
+    var __waveMode = false;
+    var __randomMode = false;
+    var __crazyMode = false;
     var drawFPS = () => {
         _.beginPath();
 
@@ -247,7 +247,7 @@ console.log(`%c[Client]`, `color: cyan;`, `bounce.js loaded!`);
             _.fillStyle = "rgba(0,0,0,0.6)";
             _.rect(0, 0, 418, 221);
             _.fill();
-            
+
             _.fillStyle = "#FFFFFF";
             _.fillText(`Press F2 to increase objects.`, 4, 42);
             _.fillText(`Press F4 to decrease objects.`, 4, 58);
@@ -258,7 +258,7 @@ console.log(`%c[Client]`, `color: cyan;`, `bounce.js loaded!`);
             _.fillText(`Press SHIFT+F4 to toggle controls.`, 4, 128);
             _.fillText(`Press SHIFT+F8 to toggle random bounce.`, 4, 144);
             _.fillText(`Press SHIFT+F9 to toggle wave mode.`, 4, 160);
-            
+
             _.fillText(`Press CTRL+F2 to toggle ball collision.`, 4, 182);
             _.fillText(`Press CTRL+F8 to toggle crazy mode.`, 4, 198);
             _.fillText(`Press CTRL+F9 to toggle splitters.`, 4, 214);
@@ -280,17 +280,17 @@ console.log(`%c[Client]`, `color: cyan;`, `bounce.js loaded!`);
             if (!__fullCross) _.fillText(`                                        [X]`, 4, 214);
         }
 
-        
+
         _.font = "16px Consolas";
         _.fillStyle = "#FFFFFF";
         var fpsString = (`${__fpsCount}`.length < 3) ? ((`${__fpsCount}`.length < 2) ? `  ${__fpsCount}` : ` ${__fpsCount}`) : `${__fpsCount}`;
-        _.fillText(`${fpsString} FPS <${BALLS.length} of ${CURHIGH} objects ${(__waveDirection>0) ? ((__waveDirection<2) ? `↑` : `↓`) : `~`}>`, 4, 20);
+        _.fillText(`${fpsString} FPS <${BALLS.length} of ${CURHIGH} objects ${(__waveDirection > 0) ? ((__waveDirection < 2) ? `↑` : `↓`) : `~`}>`, 4, 20);
 
         _.closePath()
 
     }
     setInterval(() => {
-        __fpsLast  = __fpsCount
+        __fpsLast = __fpsCount
         __fpsCount = __fpsCache;
         __fpsCache = 0;;
 
@@ -303,16 +303,16 @@ console.log(`%c[Client]`, `color: cyan;`, `bounce.js loaded!`);
     //#region WAVE MODE
     var __waveModeInit = false;
     var __waveModeState = false;
-    var __waveModeMax   = 1;
-    var __waveDirection=0;
+    var __waveModeMax = 1;
+    var __waveDirection = 0;
     var doWaveTick = () => {
         if (!__waveModeInit) __waveModeInit = true;
 
         if (__waveMode) {
-            
+
             if (!__waveModeState) {
                 CURHIGH = `${Math.floor(__waveModeMax)}`;
-                if (BALLS.length-1 < __waveModeMax) {
+                if (BALLS.length - 1 < __waveModeMax) {
                     addBall();
                     __waveDirection = 1;
                     return setTimeout(doWaveTick, 10);
@@ -332,11 +332,11 @@ console.log(`%c[Client]`, `color: cyan;`, `bounce.js loaded!`);
 
         }
         else {
-            __waveModeMax   = 1;
+            __waveModeMax = 1;
             __waveDirection = 0;
             CURHIGH = "∞";
         };
-        
+
         return setTimeout(doWaveTick, 10);
     }
     (() => {
